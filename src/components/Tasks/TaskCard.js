c889f476eafbdb9c2a41ad6d91c7917cdd4fdf8e
bpotts11 <brittneympotts@gmail.com>
2021-03-09 15:31:15 -0600

    created list and card components. got it to map through tasks and render to the dom

---
+++ src/components/Tasks/TaskCard.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Task.css"
+
+export const TaskCard = ({ task }) => (
+    <section className="task">
+        <h3 className="task__name">{task.name}</h3>
+        <div className="task__date">{task.date}</div>
+        <div className="task__completion">{task.completion}</div>
+        {/* <button onClick={() => { history.push(`/tasks/edit/${task.id}`) }}>Edit</button>
+        <button onClick={handleRelease}>Delete</button> */}
+    </section>
+)
--- src/components/Tasks/TaskList.js
+++ src/components/Tasks/TaskList.js
@@ -1,2 +1,24 @@
+import React, { useContext, useEffect } from "react"
+import { TaskContext } from "./TasksProvider"
+import { TaskCard } from "./TaskCard"
+import "./Task.css"
 
+export const TaskList = () => {
+    const { tasks, getTasks } = useContext(TaskContext)
 
+    useEffect(() => {
+        console.log("TaskList: useEffect - getTasks");
+        getTasks()
+    }, [])
+
+    return (
+        <>
+            <h2>Tasks</h2>
+            <div className="tasks">
+                {tasks.map(task => {
+                    return <TaskCard key={task.id} task={task} />
+                })}
+            </div>
+        </>
+    )
+}
