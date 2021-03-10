7bef4fb525c4bf915d7f5c2a485770b752f1386c
bpotts11 <brittneympotts@gmail.com>
2021-03-10 12:03:11 -0600

    delete button is working

--- src/components/Tasks/TaskCard.js
+++ src/components/Tasks/TaskCard.js
@@ -1,12 +1,28 @@
-import React from "react"
+import React, { useContext } from "react"
 import "./Task.css"
+import { useHistory } from "react-router-dom"
+import { TaskContext } from "./TasksProvider"
 
-export const TaskCard = ({ task }) => (
-    <section className="task">
-        <h3 className="task__name">{task.name}</h3>
-        <div className="task__date">{task.date}</div>
-        <div className="task__completion">{task.completed}</div>
-        {/* <button onClick={() => { history.push(`/tasks/edit/${task.id}`) }}>Edit</button>
-        <button onClick={handleRelease}>Delete</button> */}
-    </section>
-)
+export const TaskCard = ({ task }) => {
+    const { deleteTask } = useContext(TaskContext)
+
+    const history = useHistory();
+
+
+    const handleDelete = () => {
+        deleteTask(task.id)
+            .then(() => {
+                history.push("/tasks")
+            })
+    }
+
+    return (
+        <section className="task">
+            <h3 className="task__name">{task.name}</h3>
+            <div className="task__date">{task.date}</div>
+            <div className="task__completion">{task.completed}</div>
+            {/* <button onClick={() => { history.push(`/tasks/edit/${task.id}`) }}>Edit</button> */}
+            <button onClick={handleDelete}>Delete</button>
+        </section>
+    )
+}
