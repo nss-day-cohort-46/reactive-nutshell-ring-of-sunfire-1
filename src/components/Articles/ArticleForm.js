41ca55ad025a14223a971806837fdb29a343eb20
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-03-11 15:36:29 -0600

    Edit Feature still broke

--- src/components/Articles/ArticleForm.js
+++ src/components/Articles/ArticleForm.js
@@ -56,7 +56,7 @@ export const ArticleForm = () => {
             // .then(() => history.push("/articles"))   
 
             // Refactored code for "EDIT" feature
-            if(article.name === "") {
+            if(article.title === "") {
                 window.alert("Please complete all fields")
             }else{
                 setIsLoading(true);
@@ -100,25 +100,25 @@ export const ArticleForm = () => {
             <fieldset>
                 <div className="form-group">
                     <label htmlFor="url">Article URL:</label>
-                    <input type="text" id="url" onChange={handleControlledInputChange} required autoFocus className="formControl" placeholder="Article url" value={articles.url}/>
+                    <input type="text" id="url" onChange={handleControlledInputChange} required autoFocus className="formControl" placeholder="Article url" value={article.url}/>
                 </div>
             </fieldset>
             <fieldset>
                 <div className="form-group">
                     <label htmlFor="title">Article Title: </label>
-                    <input type="text" id="title" onChange={handleControlledInputChange} required autoFocus className="formControl" placeholder="Article Title" value={articles.title}/>
+                    <input type="text" id="title" onChange={handleControlledInputChange} required autoFocus className="formControl" placeholder="Article Title" value={article.title}/>
                 </div>
             </fieldset>
             <fieldset>
                 <div className="form-group">
                     <label htmlFor="synopsis">Article Synopsis: </label>
-                    <input type="textarea" id="synopsis" onChange={handleControlledInputChange} required autoFocus className="formControl" placeholder="Article Synopsis" value={articles.synopsis}/>
+                    <input type="textarea" id="synopsis" onChange={handleControlledInputChange} required autoFocus className="formControl" placeholder="Article Synopsis" value={article.synopsis}/>
                 </div>
             </fieldset>
             <fieldset>
                 <div className="form-group">
                     <label htmlFor="title">Article Date: </label>
-                    <input type="date" id="date" onChange={handleControlledInputChange} required autoFocus className="formControl" placeholder="" value={articles.timeStamp}/>
+                    <input type="date" id="date" onChange={handleControlledInputChange} required autoFocus className="formControl" placeholder="" value={article.timeStamp}/>
                 </div>
             </fieldset>
                 {/* <button className="saveArticle" onClick={handleClickSaveArticle}>  "FIRST SAVE BUTTON I DID"*/}
