93fa7bb96a5a999a67323c24e3e6d79d1a4c5c75
bpotts11 <brittneympotts@gmail.com>
2021-03-11 10:04:37 -0600

    new pull request

--- src/components/Tasks/TaskCard.js
+++ src/components/Tasks/TaskCard.js
@@ -3,9 +3,12 @@ import "./Task.css"
 import { useHistory } from "react-router-dom"
 import { TaskContext } from "./TasksProvider"
 
-export const TaskCard = ({ task }) => {
+export const TaskCard = ({ taskObj }) => {
     const { deleteTask, completeTask } = useContext(TaskContext)
     const history = useHistory();
+    const [task, setTask] = useState({
+        // complete: true
+    })
 
 
     const handleDelete = () => {
@@ -15,10 +18,21 @@ export const TaskCard = ({ task }) => {
             })
     }
 
+    const handleComplete = (event) => {
+        const newTask = { ...task }
+        newTask[event.target.id] = event.target.completed
+        setTask(newTask)
+        if (event.target.completed === true) {
+            const taskId = task.id
+            const completed = newTask
+            completeTask(taskId, completed)
+        } else ()
+    }
+
 
     return (
         <section className="task">
-            <input type="checkbox" id="completed" onClick="{ need to come back here and input info }></input>
+            <input type="checkbox" id="completed" onClick={handleComplete}></input>
             <h3 className="task__name">{task.name}</h3>
             <div className="task__date">{task.date}</div>
             <div className="task__completion">{task.completed}</div>
