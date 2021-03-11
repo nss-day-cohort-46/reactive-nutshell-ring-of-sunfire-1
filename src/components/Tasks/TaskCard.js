993232859ac34177cb0b0fbf28545205a8c9c140
bpotts11 <brittneympotts@gmail.com>
2021-03-11 12:21:46 -0600

    I GOT THE CHECKS TO WORK AND DISAPPEAR FROM DOM WHEN CHECKEDgs gs :q

--- src/components/Tasks/TaskCard.js
+++ src/components/Tasks/TaskCard.js
@@ -3,13 +3,11 @@ import "./Task.css"
 import { useHistory } from "react-router-dom"
 import { TaskContext } from "./TasksProvider"
 
-export const TaskCard = ({ taskObj }) => {
+export const TaskCard = ({ task }) => {
     const { deleteTask, completeTask } = useContext(TaskContext)
     const history = useHistory();
-    const [task, setTask] = useState({
-        // complete: true
-    })
-
+    const currentUserId = parseInt(sessionStorage.getItem("nutshell_user"))
+    // const [task, setTask] = useState({})
 
     const handleDelete = () => {
         deleteTask(task.id)
@@ -19,25 +17,32 @@ export const TaskCard = ({ taskObj }) => {
     }
 
     const handleComplete = (event) => {
-        const newTask = { ...task }
-        newTask[event.target.id] = event.target.completed
-        setTask(newTask)
-        if (event.target.completed === true) {
-            const taskId = task.id
-            const completed = newTask
-            completeTask(taskId, completed)
-        } else ()
+        if (event.target.checked === true) {
+            task.completed = true
+        } else {
+            task.completed = false
+        }
+        completeTask({
+            id: task.id,
+            name: task.name,
+            userId: currentUserId,
+            date: task.date,
+            completed: task.completed
+        })
     }
 
-
-    return (
-        <section className="task">
-            <input type="checkbox" id="completed" onClick={handleComplete}></input>
-            <h3 className="task__name">{task.name}</h3>
-            <div className="task__date">{task.date}</div>
-            <div className="task__completion">{task.completed}</div>
-            {/* <button onClick={() => { history.push(`/tasks/edit/${task.id}`) }}>Edit</button> */}
-            <button onClick={handleDelete}>Delete</button>
-        </section>
-    )
+    if (task.completed === true) {
+        return (<></>)
+    } else {
+        return (
+            <section className="task">
+                <input type="checkbox" id="completed" onClick={handleComplete} value="task.completed ? true : false"></input>
+                <h3 className="task__name">{task.name}</h3>
+                <div className="task__date">{task.date}</div>
+                <div className="task__completion">{task.completed}</div>
+                {/* <button onClick={() => { history.push(`/tasks/edit/${task.id}`) }}>Edit</button> */}
+                <button onClick={handleDelete}>Delete</button>
+            </section>
+        )
+    }
 }
