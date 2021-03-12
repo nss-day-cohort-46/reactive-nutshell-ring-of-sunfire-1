421f145874a40cf1e6665f54db859feea6459ec7
bpotts11 <brittneympotts@gmail.com>
2021-03-12 10:26:05 -0600

    added a home button. Created a message form and post and delete button for the messages

--- src/components/Messages/MessageCard.js
+++ src/components/Messages/MessageCard.js
@@ -19,17 +19,7 @@ export const MessageCard = ({ message }) => {
     return (
         <section className="message">
             <p>{message.textArea}</p>
-
+            <button id={message.id} onClick={handleDelete}>X</button>
         </section>
     )
 }
-
-
-
-// <div className="message__colRight">
-//     {message.userId === parseInt(sessionStorage.getItem("nutshell_user")) ?
-//         <button id={message.id} onClick={handleDelete} className="btn btn-primary">X</button>
-//         :
-//         <button className="btn btnHidden btn-primary">X</button> //hidden button for layout
-//     }
-// </div>
