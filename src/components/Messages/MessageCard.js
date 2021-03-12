cde8448029bf3121f937d539f1c252c80677f3b6
bpotts11 <brittneympotts@gmail.com>
2021-03-12 09:54:35 -0600

    working on the delete button

--- src/components/Messages/MessageCard.js
+++ src/components/Messages/MessageCard.js
@@ -1,8 +1,35 @@
-import React from "react"
+import React, { useContext } from "react"
 import "./Message.css"
+import { useHistory } from "react-router-dom"
+import { MessageContext } from "./TasksProvider"
 
-export const MessageCard = ({ message }) => (
-    <section className="message">
-        <p>{message.textArea}</p>
-    </section>
-)
+export const MessageCard = ({ message }) => {
+    const { deleteMessage } = useContext(MessageContext)
+    const history = useHistory()
+    const currentUserId = parseInt(sessionStorage.getItem("nutshell_user"))
+
+    const handleDelete = () => {
+        deleteMessage(message.id)
+            .then(() => {
+                history.push("/tasks")
+            })
+    }
+
+
+    return (
+        <section className="message">
+            <p>{message.textArea}</p>
+        </section>
+    )
+}
+
+
+
+
+<div className="message__colRight">
+    {message.userId === parseInt(sessionStorage.getItem("nutshell_user")) ?
+        <button id={message.id} onClick={handleDelete} className="btn btn-primary">X</button>
+        :
+        <button className="btn btnHidden btn-primary">X</button> //hidden button for layout
+    }
+</div>
